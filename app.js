a7de664b630093ce0d283c8258caab494f4e69eb
Shibin-b <shibinbevin123@gmail.com>
2022-07-15 15:50:49 +0530

    backend created

---
+++ app.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const app=express();
+course=[
+    {
+    "courseTitle":"React",
+    "courseDescription":"React Course",
+    "courseDuration":"5 Months",
+    "courseDate":"2022-02-23",
+    "courseVenue":"Online"
+    },
+    {
+        "courseTitle":"ExpressJS",
+        "courseDescription":"ExpressJS Course",
+        "courseDuration":"5 Months",
+        "courseDate":"2021-06-01",
+        "courseVenue":"Online"
+        },
+        {
+            "courseTitle":"NodeJS",
+            "courseDescription":"NodeJS Course",
+            "courseDuration":"4 Months",
+            "courseDate":"2022-05-11",
+            "courseVenue":"Online"
+            },
+            {
+                "courseTitle":"Angular",
+                "courseDescription":"Angular Course",
+                "courseDuration":"6 Months",
+                "courseDate":"2022-02-30",
+                "courseVenue":"Physical"
+                }
+]
+app.get('/getcourses',function(req,res){
+    res.send(course);
+})
+app.post('/addcourse',function(req,res){
+    console.log(req.body)
+    course.push(req.body);
+    req.send(course);
+})
+app.listen(3000);
