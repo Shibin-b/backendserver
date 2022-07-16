3ea95647b0c63a4cb0fcf6d15354fb21cff9b759
Shibin-b <shibinbevin123@gmail.com>
2022-07-16 16:12:03 +0530

    delete done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require("express");
+const CourseData=require('./src/model/CourseData');
 const app=express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 // course=[
 //     {
 //     "courseTitle":"React",
@@ -39,4 +41,70 @@ app.use(express.json());
 //     course.push(req.body);
 //     res.status(200).send(course);
 // })
+
+
+
+
+
+
+
+
+app.get('/getcourses',function(req,res){
+    CourseData.find().then(function(courses){
+        console.log(courses);
+        res.send(courses);
+        
+    })
+})
+
+
+
+
+app.post('/addcourse',function(req,res){
+    var item={
+        courseTitle:req.body.courseTitle,
+        courseDescription:req.body.courseDescription,
+        courseVenue:req.body.courseVenue,
+        courseDuration:req.body.courseDuration,
+        courseDate:req.body.courseDate
+    }
+    var course=CourseData(item);
+    course.save();
+
+    CourseData.find()
+    .then(function(course){
+        res.send(course);
+    })
+})
+
+
+
+app.put('/update/:id',function(req,res){
+    const id=req.params.id;
+    courseTitle=req.body.courseTitle;
+        courseDescription=req.body.courseDescription;
+        courseVenue=req.body.courseVenue;
+        courseDuration=req.body.courseDuration;
+        courseDate=req.body.courseDate;
+
+        CourseData.findByIdAndUpdate({"_id":id},
+        {$set:{"courseTitle":courseTitle,
+        "courseDescription":courseDescription,
+        "courseVenue":courseVenue,
+        "courseDuration":courseDuration,
+        "courseDate":courseDate
+}}).then(function(){res.send("Updated")});
+})
+
+
+app.delete('/delete/:id',function(req,res){
+    const id=req.params.id;
+    CourseData.findByIdAndDelete(id,function()
+    {res.send("Deleted")})
+})
+
+
+
+
 app.listen(3000);
+console.log("server running at port:3000");
